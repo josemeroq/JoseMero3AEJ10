c0e11f60850403741753d515ff80351977e4a9e3
josemeroq <JAMQ1316762374@gmail.com>
2022-05-18 22:56:06 -0500

    Agregar archivos de proyecto.

---
+++ Factorial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoseMero3AEJ10
+{
+    public class Factorial
+    {
+        private String num { get; set; }
+        private int valor { get; set; }
+
+        private int fact { get; set; }
+
+        public Factorial()
+        {
+            this.num = "";
+            this.valor = 0;
+            this.fact = 1;
+        }
+
+        public void procesofac()
+        {
+            Console.WriteLine("Inrese un número");
+            num = Console.ReadLine();
+            valor = Convert.ToInt32(num);
+            for (int i = 1; i <= valor; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine("El factorial del número " + valor + " es: " + fact);
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JoseMero3AEJ10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial facto = new Factorial();
+            facto.procesofac();
+        }
+    }
+}
